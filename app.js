1d9ec9a8eda22dddd3c55b6258748fd2defacc4a
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-01-26 15:40:35 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+//selectors
+const  todoInput =  document.querySelector('.todo-input');
+const  todoButton =  document.querySelector('.todo-button');
+const  todoList =  document.querySelector('.todo-list');
+
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//functions
+function addTodo(Event){
+    //prevent form from submitting
+    event.preventDefault();
+    //todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText ='hey';
+    newTodo.classList.add('todo-item'); 
+    todoDiv.appendChild(newTodo);
+    
+    //check mark button
+
+    const completedButton =document.createElement('button');
+    completedButton.innerText = '<i class= "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //check trash button  
+
+    const trashButton =document.createElement('button');
+    trashButton.innerText = '<i class= "fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+
+}
